3d5063a079c7a0db9580453ad37046b175368abb
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-09-26 16:34:03 +0200

    [UPD] Created "trains" table via migrations

--- README.md
+++ README.md
@@ -62,3 +62,21 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+
+Esercizio di oggi:
+nome repo: laravel-migration-seeder
+Creiamo una tabella trains e relativa Migration
+Ogni treno dovrà avere:
+Azienda
+Stazione di partenza
+Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato
+Fate attenzione ai campi stazione di partenza e stazione di arrivo... si può arrivare prima di partire? :occhiolino:
+Inserite inizialmente i dati tramite PhpMyAdmin.
+Create Model relativo ed un Controller per mostrare nella home page tutti i treni che sono in partenza dalla data odierna.
---
+++ database/migrations/2024_09_26_135019_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_partenza', 50);
+            $table->string("stazione_arrivo", 50);
+            $table->time('oraio_partenza', precision: 0);
+            $table->time('oraio_arrivo', precision: 0);
+            $table->string('codice_treno', 10);
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
